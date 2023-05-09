3e9e22211561bc9620cf589ee3d8c5da30bde32c
SejFugl <129046049+SejFugl@users.noreply.github.com>
2023-05-09 15:53:56 +0200

    Delete WinFormsApp1 directory

--- WinFormsApp1/WinFormsApp1/Form3.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form3
-    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
-
-    End Sub
-
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-
-    End Sub
-End Class
--- WinFormsApp1/WinFormsApp1/LoadData.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class LoadData
-
-End Class
