aa0301471e0820be48e6705765e6e03ce94a8406
Henrik Gade <henr556r@edu.ucl.dk>
2023-05-10 10:58:09 +0200

    Menu v.1

---
+++ Menu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getting_Real
+{
+    public class Menu
+    {
+        int input;
+        MachineRepository machineRepository = new MachineRepository();
+
+    public List<Machine> Show()
+        {
+        
+
+        Console.WriteLine("MENU:\n Vælg 1: \n Vælg 2:");
+            string inputStr = Console.ReadLine();
+            input = int.Parse(inputStr); // det ser got ud, er næsten tilbage! Hilsen Henrik
+
+
+            //    string input = Console.ReadLine();
+            //    int inputoutput;
+            //    var result = int32.TryParse(input, out inputoutput);
+
+            // int input = int.Parse(Console.Readline());
+
+            switch (input)
+            {
+
+            case 1:
+                    return machineRepository.SortByCoordinates();
+            case 2:
+                    return machineRepository.SortByWeek();
+            case 3:
+                    return machineRepository.SortByMonth();
+            case 4:
+                    return machineRepository.SortByYear();
+            default:
+                Console.WriteLine("Ugyldigt valg");
+                    return new List<Machine>();
+
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,42 +2,85 @@
 
 namespace Getting_Real
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
-        {
-            DataHandler handler = new DataHandler();
 
+        {
+            int input;
             MachineRepository machineRepository = new MachineRepository();
 
-            List<Machine> machines = new List<Machine>();
-
-            machines = handler.readExcel();
-
-            foreach (var machine in machines)
+            public List<Machine> Show()
             {
-                machineRepository.AddMachine(machine);
+
+
+                Console.WriteLine("MENU:\n Vælg 1: \n Vælg 2:");
+                string inputStr = Console.ReadLine();
+                input = int.Parse(inputStr); // det ser got ud, er næsten tilbage! Hilsen Henrik
+
+
+                //    string input = Console.ReadLine();
+                //    int inputoutput;
+                //    var result = int32.TryParse(input, out inputoutput);
+
+                // int input = int.Parse(Console.Readline());
+
+                switch (input)
+                {
+
+                    case 1:
+                        return machineRepository.SortByCoordinates();
+                    case 2:
+                        return machineRepository.SortByWeek();
+                    case 3:
+                        return machineRepository.SortByMonth();
+                    case 4:
+                        return machineRepository.SortByYear();
+                    default:
+                        Console.WriteLine("Ugyldigt valg");
+                        return new List<Machine>();
+
+                }
             }
-
-            //List<Machine> weekMachines = machineRepository.SortByWeek();
-
-            //List<Machine> monthMachines = machineRepository.SortByMonth();
-
-            //List<Machine> yearMachines = machineRepository.SortByYear();
-
-            //Console.WriteLine(WeekMachines.Count.ToString());
-            //Console.WriteLine(MonthMachines.Count.ToString());
-            //Console.WriteLine(YearMachines.Count.ToString());
-
-            List<Machine> sortedMachines = machineRepository.SortByCoordinates();
-
-            sortedMachines = machineRepository.SortByCoordinates();
-
-            foreach (var machine in sortedMachines)
-            {
-                Console.WriteLine(machine.Coordinates.ToString());
-            }
-
         }
     }
-} 
+}
+
+//        {
+//            DataHandler handler = new DataHandler();
+
+//            MachineRepository machineRepository = new MachineRepository();
+
+//            List<Machine> machines = new List<Machine>();
+
+//            machines = handler.readExcel();
+
+//            foreach (var machine in machines)
+//            {
+//                machineRepository.AddMachine(machine);
+//            }
+
+//            //List<Machine> weekMachines = machineRepository.SortByWeek();
+
+//            //List<Machine> monthMachines = machineRepository.SortByMonth();
+
+//            //List<Machine> yearMachines = machineRepository.SortByYear();
+
+//            //Console.WriteLine(WeekMachines.Count.ToString());
+//            //Console.WriteLine(MonthMachines.Count.ToString());
+//            //Console.WriteLine(YearMachines.Count.ToString());
+
+//            List<Machine> sortedMachines = machineRepository.SortByCoordinates();
+
+//            sortedMachines = machineRepository.SortByCoordinates();
+
+//            foreach (var machine in sortedMachines)
+//            {
+//                Console.WriteLine(machine.Coordinates.ToString());
+//            }
+
+
+
+//        }
+//    }
+//} 
